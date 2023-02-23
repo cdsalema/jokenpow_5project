66e6b14887501e089693b83125f412a5834426a0
cdsalema <conradod.salema@gmail.com>
2023-02-23 14:11:33 -0300

    feat - incluido switch-case statament e funções de controle

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,65 +1,123 @@
 const jogadasPossiveis = ['pedra','papel','tesoura'];
 let placarPlayer = 0;
 let placarPC = 0;
+let mensagem= "";
+let acabou = 0;
 
 function play(jogadaPlayer){
-    const jogadaRandom = Math.floor(Math.random()*3);
+   if (acabou!=1){
+    const jogadaRandom = Math.floor(Math.random()*jogadasPossiveis.length);
     const jogadaPC = jogadasPossiveis[jogadaRandom];
-   console.log("JogadaPC: "+ jogadaPC+" "+"JogadaRandom: "+ jogadaRandom);
-   console.log("JogadaPlayer: "+ jogadaPlayer);
-
-
-    if (jogadaPC===jogadaPlayer){
-        alert("EMPATE!")
+    let iconejogadaPC;
+        //console.log("JogadaPC: "+ jogadaPC+" "+"JogadaRandom: "+ jogadaRandom);
+        //console.log("JogadaPlayer: "+ jogadaPlayer);
+        
+        switch (`${jogadaPC}_${jogadaPlayer}`) {
+            case `pedra_pedra`:
+              mensagem = "EMPATE! 🤦";
+              break;
+            case `papel_papel`:
+              mensagem = "EMPATE! 🤦";
+              break;
+            case `tesoura_tesoura`:
+              mensagem = "EMPATE! 🤦";
+              break;
+            case `pedra_papel`:
+              mensagem = `GANHOU! escolhi ${PCescolha()}`;
+              placarPlayer++;
+              break;
+            case `papel_tesoura`:
+              mensagem = `GANHOU! escolhi ${PCescolha()}`;
+              placarPlayer++;
+              break;
+            case `tesoura_pedra`:
+              mensagem = `GANHOU! escolhi ${PCescolha()}`;
+              placarPlayer++;
+              break;
+            default:
+              mensagem = `PERDEU!! escolhi ${PCescolha()}`;
+              placarPC++;
+              break;
+          }vencedorPartida();
+   /** if (jogadaPC===jogadaPlayer){
+        mensagem = "EMPATE!"
     }else if (jogadaPC==="pedra"){
         if(jogadaPlayer==="papel"){
-            alert("GANHOU!")
+            mensagem =`GANHOU! escolhi ${PCescolha()}`
             placarPlayer ++;
         }
         else {
-            alert("PERDEU!!")
+            mensagem =`PERDEU!! escolhi ${PCescolha()}`
             placarPC ++
         }
     }else if(jogadaPC==="papel"){
         if(jogadaPlayer === "tesoura"){
-            alert("GANHOU!")
+            mensagem =`GANHOU! escolhi ${PCescolha()}`
             placarPlayer ++;
         }else {
-            alert("PERDEU!!")
+            mensagem =`PERDEU!! escolhi ${PCescolha()}`
             placarPC ++
         }
     }else if(jogadaPC==="tesoura"){
         if(jogadaPlayer === "pedra"){
-            alert("GANHOU!")
+            mensagem =`GANHOU! escolhi ${PCescolha()}`
             placarPlayer ++;
         }else {
-            alert("PERDEU!!")
+            mensagem =`PERDEU!! escolhi ${PCescolha()}`
             placarPC ++
-        }
-    }vencedorPartida();
+        }**/
+   
 
     function vencedorPartida(){
         if(placarPC===3 ){
-               alert ("O Vencedor foi Computador!")
-               ReiniciarPartida();  
+            mensagem =`POW! Eu ganhei!! escolhi ${PCescolha()}`
+            acabou=1;
+            document.querySelector('.opcao').innerText = "❌";
+            document.querySelector('.opcao1').innerText = "❌";
+            document.querySelector('.opcao2').innerText = "❌";
+            ReiniciarPartida();  
         }else if (placarPlayer===3){
-            alert ("Você foi o Vencedor!")
+            mensagem =`Você foi o Vencedor! escolhi ${PCescolha()}`
+            acabou=1;
+            document.querySelector('.opcao').innerText = "❌";
+            document.querySelector('.opcao1').innerText = "❌";
+            document.querySelector('.opcao2').innerText = "❌";
             ReiniciarPartida();       
+        }
+    }
+    function PCescolha(){
+        if (jogadaPC === 'pedra'){
+            iconejogadaPC = "✊";
+            return iconejogadaPC;
+        } else if (jogadaPC==='papel'){
+            iconejogadaPC= "🤚";
+            return iconejogadaPC;
+        }else if (jogadaPC==='tesoura'){
+            iconejogadaPC="✌️";
+            return iconejogadaPC;
         }
     }
     
     document.querySelector('#placarPlayer').innerText = placarPlayer;
     document.querySelector('#placarPC').innerText = placarPC;
+    document.querySelector('.mensagem').innerText = mensagem;
+    
+    
+    
+} 
 }
 
 function ReiniciarPartida(){
-    placarPC=0;
-    placarPlayer=0;
+    let timerId = setTimeout(refresh,2000);
 }
 
 function refresh(){
-    if(confirm("Vai arregar?")==true){
-        alert("frangote(a)!")
     location.reload();
+}
+
+function desistir(){
+    if(confirm("Vai arregar?")==true){
+        alert("🐔🐔🐔🐔🐔!")
+        refresh();
     }
 }
